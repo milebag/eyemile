cf44f23390bd3e153ec462307fe37fb3d2ea1e3a
zhourh <pp84123@163.com>
2017-04-19 15:04:19 +0800

    init

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
-<html>
-<header>
-  <title>Hello World!</title>
-</header>
-<body>
-  Hello World!
-</body>
-</html>
+<?php
+// Version
+define('VERSION', '2.3.0.2');
+
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
+
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit;
+}
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
+
+start('catalog');
